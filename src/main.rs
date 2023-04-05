76a81815270d73f89070568255aa15d82a12f64f
joaofreires <rmjoaovictor@gmail.com>
2023-04-05 19:52:46 -0300

    add read file support

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,39 @@ mod builtins;
 mod parser;
 mod prelude;
 mod utils;
+use std::env;
+use std::fs;
 
-use ast::evaluate;
-use parser::parse;
-use prelude::Environment;
+use prelude::LispEngine;
 use utils::read_input;
 
 fn main() {
-    let mut env = Environment::default();
-    env.init();
-
-    loop {
-        match read_input("lisp> ") {
-            Ok(input) => {
-                if input == "exit" {
+    let mut engine = LispEngine::default();
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        loop {
+            match read_input("mini-lisp> ") {
+                Ok(input) => {
+                    if input == "exit" {
+                        break;
+                    }
+                    match engine.parse(&input) {
+                        Ok(result) => println!("{}", result),
+                        Err(err) => eprintln!("Parse error: {:?}", err),
+                    }
+                }
+                Err(err) => {
+                    eprintln!("Read error: {:?}", err);
                     break;
                 }
-
-                match parse(&input) {
-                    Ok(ast) => match evaluate(ast, &mut env) {
-                        Ok(result) => println!("{}", result),
-                        Err(err) => eprintln!("Error: {:?}", err),
-                    },
-                    Err(err) => eprintln!("Parse error: {:?}", err),
-                }
             }
-            Err(err) => {
-                eprintln!("Read error: {:?}", err);
-                break;
+        }
+    } else {
+        let filename = &args[1];
+        if let Ok(file_contents) = fs::read_to_string(filename) {
+            match engine.parse(&file_contents) {
+                Ok(result) => println!("{}", result),
+                Err(err) => eprintln!("Parse error: {:?}", err),
             }
         }
     }
